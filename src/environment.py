92d1c50ae10be2cea4f59bea51cbf2d38b5cb8f0
Abner1309 <soul_abner@hotmail.com>
2026-06-19 18:06:35 -0300

    feat: implementation of the reward function

--- src/environment.py
+++ src/environment.py
@@ -1,12 +1,26 @@
 import stable_retro
+import stable_baselines3
 
-def make_custom_env(game_name, state_name):
-    actions_space = stable_retro.Actions.FILTERED
-    observation_space = stable_retro.Observations.RAM
-    custom_env = stable_retro.make(
-        game=game_name,
-        state=state_name,
-        actions=actions_space,
-        obs_type=observation_space
-    )
-    return custom_env
+env = stable_retro.make(
+    game="SuperMarioWorld-Snes-v0",
+    state="YoshiIsland1",
+    use_restricted_actions=stable_retro.Actions.FILTERED,
+    obs_type=stable_retro.Observations.RAM
+)
+
+initial_obs, initial_info = env.reset()
+
+model = stable_baselines3.PPO("MlpPolicy", env, verbose=0, tensorboard_log="./ppo_retro_logs/")
+
+action_array = [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+
+while True:
+    if action_array[0] > 1:
+        action_array[0] = 0
+        action_array[7] = 0
+    observation, reward, terminated, truncated, info = env.step(action_array)
+    action_array[0] += 1
+    action_array[7] += 1
+    if info["lives"] < 4:
+        break
+env.close()
--- src/reward.py
+++ src/reward.py
@@ -4,8 +4,11 @@ class CustomRewardWrapper(gym.RewardWrapper):
     def __init__(self, env):
         super().__init__(env)
         self.previous_coins = None
+        self.previous_yoshiCoins = None
         self.previous_lives = None
         self.previous_score = None
+        self.previous_x = None
+        self.was_dead = 0
 
     def reward(self, reward):
         if hasattr(self.env.unwrapped, 'data'):
@@ -14,18 +17,29 @@ class CustomRewardWrapper(gym.RewardWrapper):
             info = {}
 
         actual_coins = info.get("coins")
+        actual_yoshi_coins = info.get("yoshiCoins")
         actual_lives = info.get("lives")
         actual_score = info.get("score")
+        actual_x = info.get("x")
+        is_alive = info.get("dead", 0)
 
         custom_reward = reward
 
         # Reward the agent for collecting coins.
         if self.previous_coins is not None and actual_coins > self.previous_coins:
-            custom_reward += 10.0
+            custom_reward += 30.0
+
+        # Reward the agent for collecting Yoshi Coins.
+        if self.previous_yoshiCoins is not None and actual_yoshi_coins > self.previous_yoshiCoins:
+            custom_reward += 50.0
 
         # Reward the agent for increasing lives.
         if self.previous_lives is not None and actual_lives > self.previous_lives:
             custom_reward += 100.0
+
+        # Punish the agent for dying.
+        if is_alive > 0 and self.was_dead == 0:
+            custom_reward -= 100.0
 
         # Punish the agent for losing lives.
         if self.previous_lives is not None and actual_lives < self.previous_lives:
@@ -35,9 +49,16 @@ class CustomRewardWrapper(gym.RewardWrapper):
         if self.previous_score is not None and actual_score > self.previous_score:
             custom_reward += 10.0
 
+        # Reward the agent for walking forward.
+        if self.previous_x is not None and actual_x > self.previous_x:
+            custom_reward += 0.01
+
         # Updating the control variables.
         self.previous_coins = actual_coins
+        self.previous_yoshiCoins = actual_yoshi_coins
         self.previous_lives = actual_lives
         self.previous_score = actual_score
+        self.previous_x = actual_x
+        self.was_dead = is_alive
 
         return float(custom_reward)
