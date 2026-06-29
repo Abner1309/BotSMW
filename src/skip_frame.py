b4f83dffa13bd89c342a600d7a24a23939748a98
Abner1309 <soul_abner@hotmail.com>
2026-06-29 10:43:27 -0300

    feat: SkipFrameWrapper Implemented

---
+++ src/skip_frame.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+
+class SkipFrameWrapper(gym.Wrapper):
+    def __init__(self, env, skip=4):
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+        obs = None
+        reward = None
+        terminated = False
+        truncated = False
+        info = {}
+
+        for i in range(self._skip):
+            obs, reward, terminated, truncated, info = self.env.step(action)
+        return obs, reward, terminated, truncated, info
